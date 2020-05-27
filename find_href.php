0aa7174f367b51ed4b6c304afbad92d17ea9c800
Justforvps-alex <65402750+Justforvps-alex@users.noreply.github.com>
2020-05-27 09:23:26 +0500

    Update find_href.php

--- find_href.php
+++ find_href.php
@@ -21,11 +21,24 @@ $command="mysql -u root -p12345 phones -sse 'SELECT item_url FROM phones_url whe
 $test=ssh2_exec($connection, $command);
 stream_set_blocking($test, true);
 $url=fgets($test);
+echo $url;
 $command="mysql -u root -p12345 phones -sse 'DELETE FROM phones_url WHERE id=$ident'";
 ssh2_exec($connection, $command);
 //check 
 $time_sleep=rand(8,9);
-$imgContent = Curl_avito($url,$time_sleep);
+$useragent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'.rand(60,72).'.0.'.rand(1000,9999).'.121 Safari/537.36';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+$page = curl_exec($ch);
+curl_close($ch);
+echo $page;
+$imgContent=str_get_html($page);
+echo $imgContent;
+sleep($time_sleep);
 $avitoContact = new AvitoContact;
 $imgContent = explode('base64,', $imgContent)[1];
 $a = fopen('phone.png', 'wb');
