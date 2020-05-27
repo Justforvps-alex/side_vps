0dfdf0ed08d9112b80e77d92dea070acb153fa6c
Justforvps-alex <65402750+Justforvps-alex@users.noreply.github.com>
2020-05-27 09:16:26 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function Curl_avito($url,$time_sleep,$mistakes)
+function Curl_avito($url,$time_sleep)
 {
 $useragent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'.rand(60,72).'.0.'.rand(1000,9999).'.121 Safari/537.36';
 $ch = curl_init($url);
@@ -9,6 +9,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
 $page = curl_exec($ch);
+echo $page;
 $html=str_get_html($page);
 echo $html;
 curl_close($ch);
