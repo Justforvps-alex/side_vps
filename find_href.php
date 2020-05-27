d78fff3ff257fab54ceee558ce2fac5f8d947937
Justforvps-alex <65402750+Justforvps-alex@users.noreply.github.com>
2020-05-27 09:29:25 +0500

    Update find_href.php

--- find_href.php
+++ find_href.php
@@ -26,6 +26,7 @@ $command="mysql -u root -p12345 phones -sse 'DELETE FROM phones_url WHERE id=$id
 ssh2_exec($connection, $command);
 //check 
 $time_sleep=rand(8,9);
+$url='https://www.avito.ru/items/phone/1870699661?pkey=643a70c4818ac390816a30e2499155e1&vsrc=r';
 $useragent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'.rand(60,72).'.0.'.rand(1000,9999).'.121 Safari/537.36';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -35,6 +36,7 @@ curl_setopt($ch, CURLOPT_HEADER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
 $page = curl_exec($ch);
 curl_close($ch);
+sleep($time_sleep);
 echo $page;
 $imgContent=str_get_html($page);
 echo $imgContent;
