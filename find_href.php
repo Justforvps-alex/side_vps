3b3f523740361c795d133fb12e490368c71d5a9d
Justforvps-alex <65402750+Justforvps-alex@users.noreply.github.com>
2020-05-28 20:19:58 +0500

    Update find_href.php

--- find_href.php
+++ find_href.php
@@ -49,7 +49,7 @@ if ($result) {
   stream_set_blocking($test, true);
   $unique=fgets($test);
   if($unique==0){
-    $vpn=1;
+    $vpn=7;
     $command="mysql -u root -p12345 phones -sse 'INSERT INTO phones (id, number, vpn) VALUES (NULL, '$number', '$vpn')'";
     ssh2_exec($connection, $command);
   }
